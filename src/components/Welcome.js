87db79d631cd575404d28ccb621f615fc4c90336
siddartharyan <68399822+siddartharyan@users.noreply.github.com>
2020-11-08 23:31:36 +0530

    changes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,9 @@
-// write code for Welcome component here
+const Welcome = ({ name }) => {
+  return (
+    <>
+      <h1>`Hey!${name}`</h1>
+      <h2>"Welcome to Newton School."</h2>
+    </>
+  );
+};
+export default Welcome;
