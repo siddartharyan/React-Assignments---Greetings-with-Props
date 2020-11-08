b316638ac4be92c6945f636610ce55219f0c5c4b
siddartharyan <68399822+siddartharyan@users.noreply.github.com>
2020-11-08 23:40:08 +0530

    changes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ const Welcome = ({ name }) => {
     <>
       {name.length !== 0 ? (
         <div>
-          <h1>{`Hey! ${name}`}</h1>
+          <h1>{`Hey ${name}!`}</h1>
           <h2>"Welcome to Newton School."</h2>
         </div>
       ) : (
